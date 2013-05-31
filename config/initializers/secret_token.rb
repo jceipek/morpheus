bde14176990455457b47d3a2f54f275d95338556
Julian Ceipek <julian.ceipek@gmail.com>
2013-05-31 17:27:08 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Morpheus::Application.config.secret_token = '34c9c299ce2d9786fa5025b8ba0561cf990dd4e5fd6eeda78da7fafca75b0ba768a1e5ee7e5ae90308d3e039c3fae2df47f1d2eef79ec4484d578b9b882564e2'
